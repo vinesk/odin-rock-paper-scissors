828f1c371c929c282a45247b0d4f48b1147648df
vinesk <vinesk.dev@gmail.com>
2023-11-07 15:14:08 +0100

    Add a UI, first to 5 wins

--- scripts/script.js
+++ scripts/script.js
@@ -1,49 +1,54 @@
-const choices = ["rock", "paper", "scissors"];
 let playerScore = 0;
 let computerScore = 0;
 
-const capitalizeFirstLetter = (word) => {
+const camelCase = (word) => {
   return word.charAt(0).toUpperCase() + word.slice(1).toLowerCase();
 }
 
-const getPlayerChoice = () => {
-  let answer = "";
-  while (answer !== "rock" && answer !== "paper" && answer !== "scissors") {
-    answer = prompt("Choose your weapon: rock, paper or scissors?");
-    answer = answer.toLowerCase();
-  }
-  return answer;
+const getComputerChoice = () => {
+  const choices = ["rock", "paper", "scissors"];
+  return choices[Math.floor(Math.random() * choices.length)];
 }
 
-const getComputerChoice = () => choices[Math.floor(Math.random() * choices.length)];
-
 const playRound = (playerSelection, computerSelection) => {
+  const results = document.querySelector("#results");
+  
   let round = `${playerSelection} vs ${computerSelection}`;
   if (round === "rock vs scissors" || round === "paper vs rock" || round === "scissors vs paper") {
     playerScore++;
-    return `You win! ${capitalizeFirstLetter(playerSelection)} beats ${computerSelection} (score: you = ${playerScore} & computer ${computerScore})`;
+    results.textContent = `You win! ${camelCase(playerSelection)} beats ${computerSelection} (score: you = ${playerScore} & computer ${computerScore})`;
   } else if (round === "rock vs paper" || round === "paper vs scissors" || round === "scissors vs rock") {
     computerScore++;
-    return `You lose! ${capitalizeFirstLetter(computerSelection)} beats ${playerSelection} (score: you = ${playerScore} & computer ${computerScore})`;
+    results.textContent = `You lose! ${camelCase(computerSelection)} beats ${playerSelection} (score: you = ${playerScore} & computer ${computerScore})`;
   } else {
-    return `It's a tie! (score: you = ${playerScore} & computer ${computerScore})`;
+    results.textContent = `It's a tie! (score: you = ${playerScore} & computer ${computerScore})`;
   }
 }
 
 const game = () => {
-  console.log("Instructions: Play \"rock, paper, scissors\" in 5 rounds");
-  for (let i = 0 ; i < 5 ; i++) {
-    const playerSelection = getPlayerChoice();
-    const computerSelection = getComputerChoice();
-    console.log(playRound(playerSelection, computerSelection));
-  }
-  if (playerScore > computerScore) {
-    console.log("RESULT: YOU'VE WON :)");
-  } else if (playerScore < computerScore) {
-    console.log("RESULT: YOU'VE LOST :(");
-  } else {
-    console.log("RESULT: IT'S A TIE");
-  }
+  const buttons = document.querySelectorAll("button");
+  buttons.forEach(button => {
+    button.addEventListener("click", (e) => {
+      const playerSelection = e.target.textContent.toLowerCase();
+      const computerSelection = getComputerChoice();
+      playRound(playerSelection, computerSelection);
+
+      if (playerScore === 5 || computerScore === 5) {
+        const container = document.querySelector("#container");
+        container.innerHTML = "";
+        const results = document.createElement("div");
+        results.setAttribute("id", "results");
+        if (playerScore > computerScore) {
+          results.textContent = "RESULTS: YOU'VE WON :)"
+        } else if (playerScore < computerScore) {
+          results.textContent = "RESULTS: YOU'VE WON :)";
+        } else {
+          results.textContent = "IT'S A TIE";
+        }
+        container.appendChild(results);
+      }
+    });
+  });
 }
 
 game();
